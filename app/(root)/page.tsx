94bd741d14a75be18207c4e4dca1262b147b66a0
snehagaikwad <snehagaikwad18@gmail.com>
2024-10-03 18:25:33 +0530

    implement sidebar and right bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+// import RecentTransactions from '@/components/RecentTransactions';
+// import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+// import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+// import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  // const currentPage = Number(page as string) || 1;
+  const loggedIn = { firstName: "Sneha" , lastName:"Gaikwad" , email:"connect.sneha@gmail.com" };
+  // const accounts = await getAccounts({
+  //   userId: loggedIn.$id
+  // })
+
+  // if(!accounts) return;
+
+  // const accountsData = accounts?.data;
+  // const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  // const account = await getAccount({ appwriteItemId })
+
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-export default Home
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.5}
+          />
+        </header>
+
+        {/* <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:823.50} , {currentBalance:2355.3}]}
+      />
+    </section>
+  );
+};
+
+export default Home;
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionHistory = () => {
+  return (
+    <div>transaction-history</div>
+  )
+}
+
+export default TransactionHistory
